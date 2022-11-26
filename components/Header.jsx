db7752fd0ca210989a06abf69c37fbdc31dc899b
InefableDude <udgbaruch@gmail.com>
2022-11-25 19:00:34 -0600

    v.1.0.0

---
+++ components/Header.jsx
@@ -0,0 +1,44 @@
+import React, { useContext, useState, useEffect } from 'react'
+
+import Link from 'next/link'
+
+import { getCategories } from "../services";
+
+const Header = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(
+        () => {
+            getCategories().then((newCategories) => setCategories(newCategories))
+        }, []
+    )
+
+    return (
+        <div className='container mx-auto px-10 mb-8'>
+            <div className='border-b w-full inline-block border-blue-400 py-8'>
+                <div className='md:float-left block'>
+                    <Link href="/">
+                        <span className='transition duration-700 coursor-pointer hover:text-lime-400 font-bold text-4xl text-white'>
+                            Learn With Dudep
+                        </span>
+                    </Link>
+                </div>
+                <div className='hidden md:float-left md:contents'>
+                    {
+                        categories.map(
+                            (category) => (
+                                <Link key={ category.slug } href={`/category/${ category.slug }`} >
+                                    <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold transition duration-700 coursor-pointer hover:text-lime-400'>
+                                        { category.name }
+                                    </span>
+                                </Link>
+                            )
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
